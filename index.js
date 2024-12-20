b9702a87264207f9a0bbd99ef6fa31dbb1657268
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-20 08:14:12 +0600

    created testing route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { run } = require("./utils/dbconnection");
 const userRoutes = require("./routes/usersRoute");
 const jwtRoute = require("./routes/jwtRoute");
 const authorRoute = require("./routes/authorRoute");
+const verifyJWT = require("./utils/verifyJWT");
+const verifyAuthor = require("./utils/verifyAuthor");
+const verifyUser = require("./utils/verifyUser");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -24,20 +27,20 @@ app.get("/", (req, res) => {
   res.send("Server is running....");
 });
 
-// // Protected test route
-// app.get("/test-protected", verifyJWT, (req, res) => {
-//   res.send({ message: "You have access to this protected route." });
-// });
+// Protected test route
+app.get("/test-protected", verifyJWT, (req, res) => {
+  res.send({ message: "You have access to this protected route." });
+});
 
-// //only author
-// app.get("/test-doctor", verifyJWT, verifyDoctor, (req, res) => {
-//   res.send({ message: "You have access to this doctor protected route." });
-// });
+//only author
+app.get("/test-author", verifyJWT, verifyAuthor, (req, res) => {
+  res.send({ message: "You have access to this doctor protected route." });
+});
 
-// //only user
-// app.get("/test-user", verifyJWT, verifyUser, (req, res) => {
-//   res.send({ message: "You have access to this user protected route." });
-// });
+//only user
+app.get("/test-user", verifyJWT, verifyUser, (req, res) => {
+  res.send({ message: "You have access to this user protected route." });
+});
 
 app.listen(port, () => {
   console.log(`Running on port no ${port}`);
